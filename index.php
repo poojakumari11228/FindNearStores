7a350b86966f45ad8565685312985e46b0ba5382
Pooja Kumari <35136890+poojakumari11228@users.noreply.github.com>
2020-02-20 01:39:26 +0500

    index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Assignment</title>
+    <link type="text/css" rel="stylesheet" href="index.css" />
+    
+</head>
+<body>
+    
+        <div class="head">
+            <h1>Find Stores</h1>
+                <center>
+                <input type="number" name="zip-code" id="zip-code" placeholder="Enter Your Zip Code"/>
+                <button id="zip-submit">Find A Store</button>
+                </center>
+        </div>
+
+        <div id="container"></div>
+ <script src="Ajax.js"></script>
+</body>
+</html>
