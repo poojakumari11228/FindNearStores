3624daf995696944e468511f59b789decb0df00d
Pooja Kumari <35136890+poojakumari11228@users.noreply.github.com>
2020-02-20 17:17:26 +0500

    get data to calculate distance

--- FindStores.php
+++ FindStores.php
@@ -1,3 +1,14 @@
+<!-- FORMATE OF DIST ARRAY
+     dist = array(
+         "zip_code" => "distance"
+     ) 
+
+ -->
+<!-- FORMATE OF DETAILS ARRAY 
+    detail = array(
+        "zip_code" => [name, address, city, state, phone]
+    )
+ -->
 
 <?php
 
@@ -20,6 +31,31 @@ $details = array();
 if (empty($code)) {
     echo '<h1>Please Enter the Zip Code!</h1>';
 } else {
+
+    // get stores of entered zip code
+    $sql = "SELECT * FROM stores right join zip_codes on zip_codes.zip_code = stores.zip_code where zip_codes.zip_code=$code";
+    $result = $conn->query($sql);
+    //If that zip code exixts
+    if ($conn->affected_rows > 0) {
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        $lat_a = $row['latitude'];
+        $long_a = $row['longitude'];
+
+        
+    // get stores for calculation the distance
+    $sql2 = "SELECT * FROM stores right join zip_codes on zip_codes.zip_code = stores.zip_code";
+    $result2 = $conn->query($sql2);
+
+    if ($conn->affected_rows > 0) {
+        while ($row2 = $result2->fetch_array(MYSQLI_ASSOC)) {
+            $lat_b = $row2['latitude'];
+            $long_b = $row2['longitude'];
+
+              }
+    }
+ 
+    }
+    else echo '<h1>Data of entered zip code is not found in database.</h1>';
 
 }
 
